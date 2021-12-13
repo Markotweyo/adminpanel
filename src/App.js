f0c30548151d3a12e14e74c308782cdb79c38ca3
Mark Otweyo <markotweyo99@gmail.com>
2021-12-14 02:07:47 +0300

    Added sidebar menu UI component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Sidebar from "./components/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
+import './app.css'
 
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <div className="container">
+      <Sidebar/>
+      
+      </div>
+      
       
     </div>
   );
